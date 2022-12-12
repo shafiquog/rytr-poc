ae0a5dea33e9516d816fe4bf987c79fcacd664d4
Aftab shah <shahaftab092@gmail.com>
2022-12-12 17:32:31 +0500

    prompt enhancement

--- src/app/component/rytr/rytr.component.ts
+++ src/app/component/rytr/rytr.component.ts
@@ -12,11 +12,15 @@ export class RytrComponent implements OnInit , OnDestroy{
 
   editor!: Editor;
   html:any;
-context = '';
-topic = '';
+  context = '';
+  topic = '';
 
 
   data:any
+
+
+
+
 
 
   constructor(private http : HttpClient) { }
@@ -29,21 +33,61 @@ topic = '';
 
 
   }
+  getcontext() {
+    if(this.context == 'Business Idea'){
+      this.Businessidea = true;
+      this.hidetopic = false;
+    }else{
+      this.Businessidea = false;
+    }
+  }
+
   public isData = true;
+  interest: any;
+  skill: any;
+  Businessidea = false;
+  hidetopic = true;
   getValue() {
     console.log('context', this.context);
     console.log('topic', this.topic);
     this.isData = false;
-    let data = {
-      "context": this.context,
-      "topic": this.topic
+
+    if(  this.context == 'Business Idea' ) {
+
+
+      this.data = {
+        "context": 'suggest me the' + ' ' + this.context + 'for'+this.interest + 'and'+ this.skill,
+
+      }
     }
-  this.http.post("https://technoversesms.com/openai-api/api/ai",data).subscribe(
-    (res:any) =>{
-      console.log(res);
-      this.isData = true;
-      this.html = res.data;
-    });
+      if(this.context == 'Email') {
+        this.Businessidea = false;
+        this.data = {
+          "context": 'write an' + ' ' + this.context + ' ' + 'for',
+          "topic": this.topic
+        }
+      }
+    if(this.context == 'Job Description' || this.context == 'Cover Letter'){
+      this.Businessidea = false;
+      this.data = {
+        "context": 'write a ' + ' ' + this.context + ' ' + 'for',
+        "topic": this.topic
+      }
+    }
+
+
+
+
+
+
+    this.http.post("https://technoversesms.com/openai-api/api/ai",this.data).subscribe(
+      (res:any) =>{
+        console.log(res);
+        this.isData = true;
+        this.html = res.data;
+      });
+
+
 
     }
 
