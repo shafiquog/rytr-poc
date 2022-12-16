07cc049410dea2fe897e2385a3055fa9bd1b3b05
sarmad51214 <sarmadsyedali@gmail.com>
2022-12-16 17:21:13 +0500

    login form

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-
+import { EmailValidator, FormBuilder, FormGroup, Validators } from "@angular/forms";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  mailHide = true;
+ 
+  userForm: FormGroup | any;
 
-  constructor() { }
+  constructor(private fb: FormBuilder,) {
+    this.userForm = this.fb.group({
+      email: ['',Validators.required],
+      password: ['',Validators.required],
+    
 
-  ngOnInit(): void {
+    });
+    console.log('values', this.userForm.values)
+    
   }
 
+  ngOnInit(): void {
+    this.show_Login();
+    localStorage.setItem("email", "Sarmadsyedali@gmail.com");
+  }
+  show_Login() {
+ 
+    this.mailHide = !this.mailHide;
+ 
+  }
+
+  onSubmit() {
+ 
+    if (this.userForm.invalid) {
+      alert('Please fill Form')
+    }
+    else {
+      alert('Sucess')
+    }
+ 
+  }
 }
